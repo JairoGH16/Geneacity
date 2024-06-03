328aae66f060f1e5fc9b4c21575c885e2c455817
JairoGH16 <162930229+JairoGH16@users.noreply.github.com>
2024-06-03 12:29:22 -0600

    Actualizacion árbol genealógico
    Ahora se pueden insertar todo tipo de tíos.

--- arbol_genealogico.py
+++ arbol_genealogico.py
@@ -1,17 +1,15 @@
-import consultas
 from nodos_arbol import Nodo_persona
 
-class Arbol_genealogico:
-    def insertar_personas_casa(self,raiz,lista_personas):
-        lista_personas
+class Arbol_genealogico_insertador:
+    def insertar_personas_casa(self,raiz:Nodo_persona,lista_personas:list['Nodo_persona']):
         for persona in lista_personas:
             self.__insertar_persona(raiz,persona)
-        pass
-    #Los hijos no están porque estos se agregan desde Agregador_familiares_directos inmediatamente cuando nacen.
     def __insertar_persona(self,raiz,persona):
-        if self.__insertar_ascendencia(raiz,persona)==True:
+        if self.__insertar_hermanos_tios(raiz,persona):
             return True
-        elif self.__insertar_hermanos(raiz,persona)==True:
+        elif self.__insertar_ascendencia(raiz,persona):
+            return True
+        elif self.__insertar_descendencia(raiz,persona):
             return True
         return False
     def __insertar_ascendencia(self,raiz:Nodo_persona,persona:Nodo_persona):
@@ -27,30 +25,49 @@ class Arbol_genealogico:
             elif self.__insertar_ascendencia(raiz.madre,persona)==True:
                 return True
             return False
-    def __insertar_hermanos(self,raiz:Nodo_persona,persona:Nodo_persona):
+    def __insertar_hermanos_tios(self,raiz:Nodo_persona,persona:Nodo_persona):
         if raiz:
             if (raiz.padre_id == persona.padre_id and raiz.padre_id != 0) or (raiz.madre_id == persona.madre_id and raiz.madre_id != 0):
                 if persona not in raiz.hermanos:
                     raiz.hermanos.append(persona)
                 if raiz not in persona.hermanos:
                     persona.hermanos.append(raiz)
+                    return True
+            elif self.__insertar_hermanos_tios(raiz.padre,persona):
+                return True
+            elif self.__insertar_hermanos_tios(raiz.madre,persona):
+                return True
+            return False
+    def __insertar_descendencia(self,raiz:Nodo_persona,persona:Nodo_persona):
+        if raiz:
+            if raiz.persona_id==persona.padre_id:
+                raiz.hijos.append(persona)
+                persona.padre=raiz
+                return True
+            elif raiz.persona_id==persona.madre_id:
+                raiz.hijos.append(persona)
+                persona.madre=raiz
+                return True
+            elif len(raiz.hijos) > 0:
+                for hijo in raiz.hijos:
+                    if self.__insertar_descendencia(hijo,persona)==True:
+                        return True
+            return False
             
-Luis=Nodo_persona(20,1,2,"Luis","Male",23,"Single")
-Juan=Nodo_persona(30,1,2,"Juan","Male",23,"Single")
-#Padres
-Miguel=Nodo_persona(1,3,4,"Miguel","Male",43,"Single")
-Maria=Nodo_persona(2,5,6,"Maria","Female",40,"Single")
-Luis.madre=Maria
-Luis.padre=Miguel
-Juan.padre=Miguel
-Juan.madre=Maria
-#Abuelos paternos
-Mauricio=Nodo_persona(3,7,8,"Mauricio","Male",60,"Single")
-Ana=Nodo_persona(4,9,10,"Ana","Male",58,"Single")
-#Abuelos maternos
-Hernando=Nodo_persona(5,11,12,"Hernando","Male",70,"Single")
-Leticia=Nodo_persona(6,13,14,"Leticia","Male",68,"Single")
-casa_lista=[Juan]
-arbol=Arbol_genealogico()
-arbol.insertar_personas_casa(Luis,casa_lista)
+#BISABUELO
+Rodolfo=Nodo_persona(1,0,12,"Rodolfo","Male",80,"Single")
+Lucho=Nodo_persona(23,0,12,"Lucho","Male",70,"Married")
+#ABUELO
+Pablo=Nodo_persona(2,1,11,"Pablo","Male",55,"Single")
+#PADRE
+ernesto=Nodo_persona(3,2,10,"Ernesto","Male",25,"Single")
+#PERSONA
+luis=Nodo_persona(4,3,9,"Luis","Male",5,"Single")
+#HIJO
+pedro=Nodo_persona(22,4,89,"Pedro","Male",1,"Married")
+arbol=Arbol_genealogico_insertador()
+arbol.insertar_personas_casa(luis,[ernesto,Pablo])
+arbol.insertar_personas_casa(luis,[Rodolfo])
+arbol.insertar_personas_casa(luis,[pedro])
+arbol.insertar_personas_casa(pedro,[Lucho])
 pass
