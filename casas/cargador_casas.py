1b54a5a115a5b1dfba90e825bae9671864586f38
JairoGH16 <gonzalezhidalgojairojesus@gmail.com>
2024-06-06 00:03:06 -0600

    Movimiento
    Las casas ya tienen movimiento

--- casas/cargador_casas.py
+++ casas/cargador_casas.py
@@ -10,20 +10,23 @@ class Cargador_casas:
         # Pre-cargar la imagen para mejorar el rendimiento
         self.edificio_ciudad = pygame.image.load('casas/ciudad.png')
 
-    def tecla_presionada(self, key, last_action_time, screen):
+    def tecla_presionada(self, key, last_action_time,personaje_x,personaje_y):
         if key not in last_action_time:
             last_action_time[key] = time.time()
-            self.cargar_casas()
+            self.recargar_casas(personaje_x,personaje_y)
 
     def tecla_alzada(self, key, last_action_time):
         if key in last_action_time:
             del last_action_time[key]
 
-    def cargar_casas(self):
-        self.lista_casas=(consultas.Consulta_casas_cercanas.consultar_casas(300, 300))
-        print(self.lista_casas)
+    def recargar_casas(self,personaje_x,personaje_y):
+        try:
+            self.lista_casas=(consultas.Consulta_casas_cercanas.consultar_casas(personaje_x,personaje_y))
+            print(self.lista_casas)
+        except:
+            print("No hay casas")
 
-    def dibujar_casas(self,screen):
+    def dibujar_casas(self,screen,personaje_x,personaje_y):
         """
         Dibuja una imagen en la pantalla de Pygame en la posición (x, y).
 
@@ -31,5 +34,9 @@ class Cargador_casas:
         - screen: superficie de Pygame donde se dibuja la imagen.
         - x, y: coordenadas donde se dibujará la imagen.
         """
-        for casa in self.lista_casas: # Dibuja la imagen de cada casa
-            screen.blit(self.edificio_ciudad, (int(casa["x"]), int(casa["y"])))
+        centro_x = screen.get_width() / 2
+        centro_y = screen.get_height() / 2
+        for casa in self.lista_casas:
+            casa_x = float(centro_x) + float(casa["x"]) - float(personaje_x)
+            casa_y = float(centro_y) + float(casa["y"]) - float(personaje_y)
+            screen.blit(self.edificio_ciudad, (casa_x, casa_y))
--- main.py
+++ main.py
@@ -10,12 +10,25 @@ class Game:
         self.running = True
         self.last_action_time = {}
         self.cargador_casas = Cargador_casas()
-        self.admin_personajes=Administrador_personajes(self.screen,50,"Female") #ESTO HAY QUE CONSULTARLO
+        self.admin_personajes=Administrador_personajes(self.screen,17,"Male") #ESTO HAY QUE CONSULTARLO
         self.moviendose=False
+        self.personaje_x=300
+        self.personaje_y=300
 
     def run(self):
         self.admin_personajes.actualizar_personaje(115)
         while self.running:
+
+            keys_pressed = pygame.key.get_pressed()  # Revisar el estado de las teclas fuera de los eventos
+            if keys_pressed[pygame.K_w] or keys_pressed[pygame.K_UP]:
+                self.personaje_y -= 15
+            if keys_pressed[pygame.K_s] or keys_pressed[pygame.K_DOWN]:
+                self.personaje_y += 15
+            if keys_pressed[pygame.K_a] or keys_pressed[pygame.K_LEFT]:
+                self.personaje_x -= 15
+            if keys_pressed[pygame.K_d] or keys_pressed[pygame.K_RIGHT]:
+                self.personaje_x += 15
+
             self.handle_events()
             self.repeat_actions()
             pygame.time.delay(100)  # Pausa de 100 milisegundos en el bucle para reducir el uso de CPU.
@@ -23,7 +36,7 @@ class Game:
             if self.moviendose:
                 self.admin_personajes.actualizar_indice_personaje()
             self.admin_personajes.dibujar_personaje()
-            self.cargador_casas.dibujar_casas(self.screen)
+            self.cargador_casas.dibujar_casas(self.screen,self.personaje_x,self.personaje_y)
             pygame.display.flip()  # Actualizar la pantalla
         pygame.quit()
 
@@ -34,7 +47,7 @@ class Game:
             elif event.type == pygame.KEYDOWN:
                 self.moviendose=True
                 if event.key in [pygame.K_w, pygame.K_a, pygame.K_s, pygame.K_d, pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
-                    self.cargador_casas.tecla_presionada(event.key, self.last_action_time,self.screen)
+                    self.cargador_casas.tecla_presionada(event.key,self.last_action_time,self.personaje_x,self.personaje_y)
                     self.admin_personajes.actualizar_personaje(event.key)
             elif event.type == pygame.KEYUP:
                 keys_pressed = pygame.key.get_pressed()  # Retorna un array de bools para cada tecla
@@ -49,7 +62,7 @@ class Game:
         for key in keys_to_check:
             if key in self.last_action_time and (current_time - self.last_action_time[key]) >= 5:
                 self.last_action_time[key] = current_time
-                self.cargador_casas.cargar_casas()
+                self.cargador_casas.recargar_casas(self.personaje_x,self.personaje_y)
 
 if __name__ == "__main__":
     game = Game()
